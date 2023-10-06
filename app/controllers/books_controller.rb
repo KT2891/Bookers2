5722c4558b861be004ce7fba30633341ce37e1fa
Keishiro Takata <t.k.6.2.1995@gmail.com>
2023-10-06 01:48:42 +0000

    [Pass] spec test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+before_action :is_matching_login_user, only: [:edit, :update]
 
   def index
     @books = Book.all
@@ -9,6 +10,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    @user = current_user
     if @book.save
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book)
@@ -19,10 +21,27 @@ class BooksController < ApplicationController
   end
 
   def show
+    @create_book = Book.new
     @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Book.find(params[:id]).destroy
+    redirect_to books_path
   end
 
 # ストロングパラメータ設定
@@ -30,6 +49,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
   end
 
 end
